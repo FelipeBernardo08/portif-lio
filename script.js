248da37d34c87ec5b30b3b6d2dfb871fbddfcd38
Felipe Bernardo <felipebernardo55@hotmail.com>
2024-04-02 22:46:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+window.addEventListener('scroll', function () {
+    animeScroll();
+})
